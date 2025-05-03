2a5a02d90683ec0af9b4a176a13828b97013396b
JuanLopez <mainhead@gmail.com>
2025-05-03 13:02:12 +0200

    Fetches draft data using XPath
    Modifies the script to extract draft data from the NFL website using XPath queries. It waits for specific elements to load and then parses their text content. The team selection is temporarily commented out, and the script now focuses on retrieving and printing data related to draft picks. Also it adds an array for datapicks and temporarily disables the yaml output.

--- src/alternateselenium.py
+++ src/alternateselenium.py
@@ -13,6 +13,7 @@ options.add_argument("--headless")  # Run in headless mode (no GUI)
 #options.add_argument("--no-sandbox")  # Bypass OS security
 driver = webdriver.Chrome(service=service, options=options)
 teams = []
+dataPicks = []
 
 # Specify the path to your ChromeDriver (or other browser driver)
 # If it's in your PATH, you can skip this line
@@ -25,12 +26,19 @@ teams = []
 driver.get("https://www.nfl.com/draft/tracker/teams/buffalo-bills/2025")
 try:
     # Esperar hasta que el elemento esté presente
-    selectTeam = WebDriverWait(driver, 30).until(
+    """ selectTeam = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.NAME, "team"))
+    )"""
+    dataPicks= WebDriverWait(driver, 40).until(
+        #EC.visibility_of_element_located((By.XPATH, '//*[@data-test-id="facemask-column"]'))
+        EC.presence_of_all_elements_located((By.XPATH, '//*[@data-test-id="facemask-column"]'))
     )
-    teams = selectTeam.text.split("\n")
-    print(f"tipo de elemento: {type(teams)}")
-    print(f"Elemento encontrado: {teams}")
+    print(f"DATA PICKS: {dataPicks[3].text}")
+    picks = dataPicks[3].text.split("Rnd")
+    #teams = selectTeam.text.split("\n")
+    #print(f"tipo de elemento: {type(teams)}")
+    #print(f"Elemento encontrado: {teams}")
+    print(f"Elemento encontrado: {picks}")
 except Exception as e:
     print(f"Error: {e}")
 finally:
@@ -41,8 +49,8 @@ finally:
 
 data = {"teams": [{"name": team, "slug": team.lower().replace(" ", "-")} for team in teams]}
 
-with open("teams.yaml", "w") as file:
+""" with open("teams.yaml", "w") as file:
     yaml.dump(data, file, default_flow_style=False)
 
 print("YAML file created successfully!")
-
+"""
